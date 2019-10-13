944df8361c59069c1b50847c8b22940f54913e43
shinriyo <shinriyo@gmail.com>
2019-10-13 15:08:10 +0900

    prepare commands bm, bmadd

--- bm.fish
+++ bm.fish
@@ -1,8 +1,22 @@
 #!/usr/local/bin/fish
 
+function __bm_get
+#   escaped_bmid=$(echo "$1" | sed -e 's/[^a-zA-Z0-9<>]/\\&/g')
+#   cat $BMRC | grep -E "^${escaped_bmid}\\|" | cut -d '|' -f 2 | head -n 1
+end
+
 function bm
-    if test -z "$branch"
-        set branch master
-    end
-    curl -s https://raw.githubusercontent.com/$owner/$repo/$branch/README.md
+#   local bmid=$1
+#   if test -z "$bmid" ; then
+#     bmls
+#     return
+#   end
+
+#   local bmdir=$(__bm_get "$bmid")
+#   if test -z "$bmdir" ; then
+#     echo "$bmid is not in bm list" 1>&2
+#     return 1
+#   end
+
+#   cd "$bmdir"
 end
--- bmadd.fish
+++ bmadd.fish
@@ -1,8 +1,38 @@
 #!/usr/local/bin/fish
 
+function __bm_path_list
+  cat $BMRC | cut -d '|' -f 2
+end
+
+function __bm_id_list
+  cat $BMRC | cut -d '|' -f 1
+end
+
 function bmadd
-    if test -z "$branch"
-        set branch master
-    end
-    curl -s https://raw.githubusercontent.com/$owner/$repo/$branch/README.md
+#  local bmdir=$PWD
+#   local newid=$1
+
+#   if __bm_path_list | grep -F -x "$bmdir" > /dev/null; then
+#     echo "$bmdir is already in bm list" 1>&2
+#     return 2
+#   end
+
+#   if [ -n "$newid" ]; then
+#     if __bm_id_list | grep -F -x "$newid" > /dev/null; then
+#       echo "$newid is already used as bm id" 1>&2
+#       return 3
+#     end
+#   end
+
+#   if test -z $newid ; then
+#     local maxid
+#     maxid=$(__bm_id_list | grep '^[0-9][0-9]*$' | sort -n -r | head -1)
+#     if [ -n "$maxid" ] && [ "$maxid" -ge 1 ]; then
+#       newid=$((maxid + 1))
+#     else
+#       newid=1
+#     end
+#   end
+
+#   echo "$newid|$bmdir" >> $BMRC
 end
